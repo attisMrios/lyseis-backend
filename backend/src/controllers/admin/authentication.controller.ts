81d6d334741f519b7ff50b3b9d5617f6dd3f91ad
juan manuel de los rios <attiscolombia@gmail.com>
2022-07-24 22:56:50 -0500

    update token

--- backend/src/controllers/admin/authentication.controller.ts
+++ backend/src/controllers/admin/authentication.controller.ts
@@ -7,7 +7,7 @@ authRouter.post('/token', async (req, res) => {
     try {
         let busines = new AuthenticationBusiness();
         let token = await busines.GetToken(req.body.user, req.body.password);
-        res.status(200).send(token);
+        res.status(200).send({token: token});
     } catch (error) {
         res.status(401).send(`Sorry you are not authorized`)
     }
