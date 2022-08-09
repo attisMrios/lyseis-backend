65fe10ddcb99080cb68f33fbd95f288bfd1276e8
Juan Manuel de los Ríos <mrios@gmail.com>
2022-08-09 12:02:41 -0500

    uupload file backend

--- src/controllers/common/files.controller.ts
+++ src/controllers/common/files.controller.ts
@@ -1,22 +1,33 @@
 import express from 'express';
 import multer from 'multer'
+import * as fs from 'fs';
+import Utils from '../../utils';
 
-const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (_req, file, cb) => {
-        console.log(_req);
-        
-        cb(null, file.fieldname)
-    }
-})
 
-const upload = multer({storage: storage})
+
+const upload = multer({dest: 'uploads'})
 
 const files_router = express.Router();
-files_router.post('/productimage', upload.single('productimage'), (_req, res) =>{
-    res.send({message: "all is ok"})
+files_router.post('/productimage', upload.single('file'), (req, res) =>{
+    try {
+        // mueve el archivo al lugar indicado
+        fs.rename(req.file?.path as any, `uploads/products/${req.body.name}`,(err) => {
+            if(err){
+                res.status(500).send({message: err.message})
+            }
+        })
+        res.status(200).send({message: "all is ok"})
+    } catch (error: any) {
+        let response = {
+            message: error.message,
+            data: `An error occurred when saving file ${req.body.name} \n
+            data: ${JSON.stringify(req.body)} \n
+            Error: ${error}`
+        }
+        Utils.WriteLog(`An error occurred when saving file ${req.body.name} \n
+        data: ${JSON.stringify(req.body)} \n
+        Error: ${error.message}`);
+        res.status(500).send(response);
+    }
 });
-
 export default files_router;
